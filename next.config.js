595aa73f377e43df5e9197089e68b6f311784d0a
unknown <phamrachel17@yahoo.com>
2025-09-10 21:45:53 -0400

    fixed linting issues

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  basePath: '/portfolio2025',
-  assetPrefix: '/portfolio2025'
+  ...(isProd && {
+    basePath: '/portfolio2025',
+    assetPrefix: '/portfolio2025'
+  })
 }
 
 module.exports = nextConfig
